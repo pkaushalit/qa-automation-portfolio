b22c168ba01f26403937653bd30db585e25a2b01
pkaushalit <pkaushal.it@gmail.com>
2025-04-09 11:38:53 -0400

    GoogleSearchTest.java

---
+++ selenium-web-automation/GoogleSearchTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+        searchBox.submit();
+
+        System.out.println("Page Title is: " + driver.getTitle());
+        driver.quit();
+    }
+}
